a7d3dbf313e40c1a62d6b636500dec214ea268ad
RunalTembhurne <tembhurnerunal@gmail.com>
2021-04-15 17:36:18 +0530

    Uploading code - Home page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Go - Guide</h1>
+          <p className="lead">
+            Create a tourist profile, search destinations and get connect with local guides
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
+
+export default Landing
